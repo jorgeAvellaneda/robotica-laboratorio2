d01888001d3523de5737b21f5e44d9da5a3c97e1
jalea98 <joaavellanedaro@unal.edu.co>
2021-07-25 02:28:25 -0500

    gui y arreglos

--- inversa_toolbox.m
+++ inversa_toolbox.m
@@ -1,10 +1,10 @@
 r=-16.4; p=9.7; ya=-151.7;
 x=326.4; y=352.3; z=412.2;
-T=rt2tr(rotx(ya,'deg')*roty(p,'deg')*rotz(r,'deg'),[x y z]);
+%T=rt2tr(rotx(ya,'deg')*roty(p,'deg')*rotz(r,'deg'),[x y z]);
 
 %bot1.ikine6s(T) %solo se puede para DH estandar
-rad2deg(bot1.ikine(T)) %
-rad2deg(bot1.ikunc(T)) %No tiene en cuenta los limites de las articulaciones
-rad2deg(bot1.ikcon(T)) %Tiene en cuenta los limites de las articulaciones
-Cinem_inversa(T)        %Nuestra herramienta
+ikine = rad2deg(bot1.ikine(T)) %
+ikunc = rad2deg(bot1.ikunc(T)) %No tiene en cuenta los limites de las articulaciones
+ikcon = rad2deg(bot1.ikcon(T)) %Tiene en cuenta los limites de las articulaciones
+nuestra = Cinem_inversa(T)        %Nuestra herramienta
 %bot1.ikine_sym(6) %symbolic, opcion 6 da posiciones y rotaciones, es experimental
